8680b9d6976251de33db906b2770b0d092e9a318
olaj2 <olink.olaj2@gmail.com>
2023-03-22 15:14:53 -0700

    10. a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - sum function
+ * dscription: function that adds two integers and returns the result.
+ * @m: m parameter
+ * @n: n parameter
+ *
+ * Return: add
+ */
+int add(int m, int n)
+{
+	return (m + n);
+}
