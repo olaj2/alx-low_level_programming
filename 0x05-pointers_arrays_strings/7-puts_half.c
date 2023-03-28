4cc61e220b26c54b5e24cbf51d1d1c4f9033e43e
olaj2 <olink.olaj2@gmail.com>
2023-03-28 10:01:25 -0700

    function that prints half of a string, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - display half of a string
+ * @str: Half String to be printed
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int j = 0;
+	int k;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	if (j % 2 == 1)
+	{
+		k = (j - 1) / 2;
+		k += 1;
+	}
+
+	else
+	{
+		k = j / 2;
+	}
+}
