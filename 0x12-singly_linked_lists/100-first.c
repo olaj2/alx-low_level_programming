c2ed7f611773bc1bcdefa73be693850536ab369a
olaj2 <olink.olaj2@gmail.com>
2023-04-29 05:08:32 +0100

    Task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,7 @@ void first(void) __attribute__ ((constructor));
  * first - prints a sentence before the main
  * function is executed
  */
+
 void first(void)
 {
 		printf("You're beat! and yet, you must allow,\n");
